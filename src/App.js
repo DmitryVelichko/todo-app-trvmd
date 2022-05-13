8c2e6ca09296bfb3f631b4212a675afd80c6ea38
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-14 00:04:03 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks';
+import {useState} from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Get 1000 USD',
+        day: 'Feb 5th at 2:30',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Get 2000 USD',
+        day: 'Feb 6th at 12:30',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Get 3000 USD',
+        day: 'Feb 7th at 5:00',
+        reminder: false,
+    }
+])
   return (
     <div className="container">
-      <Header />
+      <Header title='Task Tracker'/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
