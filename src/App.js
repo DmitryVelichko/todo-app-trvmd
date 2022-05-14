65a046f42b66d442e5c280402b15c78d42ffe334
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-14 19:26:51 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import { useState } from 'react';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -31,12 +32,17 @@ function App() {
 
   // Toggle Reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task));
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className='container'>
       <Header title='Task Tracker' />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
