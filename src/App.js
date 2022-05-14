084b4cb45997870b58e0a435f9e8a90f274b7df6
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-14 13:54:32 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import Header from './components/Header'
+import Header from './components/Header';
 import Tasks from './components/Tasks';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
   const [tasks, setTasks] = useState([
     {
-        id: 1,
-        text: 'Get 1000 USD',
-        day: 'Feb 5th at 2:30',
-        reminder: true,
+      id: 1,
+      text: 'Get 1000 USD',
+      day: 'Feb 5th at 2:30',
+      reminder: true,
     },
     {
-        id: 2,
-        text: 'Get 2000 USD',
-        day: 'Feb 6th at 12:30',
-        reminder: true,
+      id: 2,
+      text: 'Get 2000 USD',
+      day: 'Feb 6th at 12:30',
+      reminder: true,
     },
     {
-        id: 3,
-        text: 'Get 3000 USD',
-        day: 'Feb 7th at 5:00',
-        reminder: false,
-    }
-])
+      id: 3,
+      text: 'Get 3000 USD',
+      day: 'Feb 7th at 5:00',
+      reminder: false,
+    },
+  ]);
 
   // Delete Task
 
   const deleteTask = (id) => {
-    console.log('delete', id)
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
+  
   return (
-    <div className="container">
-      <Header title='Task Tracker'/>
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+    <div className='container'>
+      <Header title='Task Tracker' />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
