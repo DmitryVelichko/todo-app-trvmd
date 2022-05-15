7e09d25dab235a2417d5074ffaabdcafcf65776b
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-15 22:45:26 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks');
-      const data = await res.json();
-    }
-
     fetchTasks();
-  })
+  }, []);
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  };
 
   // Add Task
   const addTask = (task) => {
